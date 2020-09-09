8c706cc349e559b9f340c0ee742ad15f9b6b7172
Brian Kubes <briankubes@gmail.com>
2020-09-08 20:33:11 -0700

    done

--- src/App.js
+++ src/App.js
@@ -5,18 +5,33 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, { useState } from "react";
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
 // Import the dummyData
-import './App.css';
+import "./App.css";
+import Posts from "./components/Posts/Posts";
+import dummyData from "./dummy-data";
+import LikeSection from "./components/Posts/LikeSection";
+import SearchBar from "./components/SearchBar/SearchBar";
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+  const [posts, setPosts] = useState(dummyData);
+  // console.log("posts", posts);
+  const likePost = (postId) => {
+    setPosts(
+      posts.map((item) => {
+        if (postId === item.id) {
+          return { ...item, likes: item.likes + 1 };
+        }
+        return item;
+      })
+    );
+  };
 
-  const likePost = postId => {
-    /*
+  /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
 
       The state of the app lives at the top of the React tree, but it wouldn't be fair for nested components not to be able to change state!
@@ -27,12 +42,14 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
-  };
 
   return (
-    <div className='App'>
-      {/* Add SearchBar and Posts here to render them */}
+    <div className="App">
+      {/* search bar and maybe something about posts*/}
+      <SearchBar />
+      <Posts posts={posts} likePost={likePost} />
       {/* Check the implementation of each component, to see what props they require, if any! */}
+      {/* <LikeSection likePost={likePost} numberOfLikes={posts.likes} /> */}
     </div>
   );
 };
--- src/components/Posts/PostHeader.js
+++ src/components/Posts/PostHeader.js
@@ -1,18 +1,14 @@
 // You do not need to change any code in this file
-import React from 'react';
+import React from "react";
 
-const PostHeader = props => {
+const PostHeader = (props) => {
   // 🔥 Make sure the parent of PostHeader is passing the right props!
   const { thumbnailUrl, username } = props;
-
+  // console.log("PostHeader", props);
   return (
-    <div className='post-header'>
-      <div className='post-thumb-wrapper'>
-        <img
-          alt='post header'
-          className='post-thumb'
-          src={thumbnailUrl}
-        />
+    <div className="post-header">
+      <div className="post-thumb-wrapper">
+        <img alt="post header" className="post-thumb" src={thumbnailUrl} />
       </div>
       <h2>{username}</h2>
     </div>
